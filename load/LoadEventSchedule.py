a0be4d8fc20807e82bd30e887d69cc8715ea7474
parameshwarbhat411 <parameshwar.bhat@gwmail.gwu.edu>
2024-07-29 11:39:32 -0400

    create dynamoDB table method added

--- load/LoadEventSchedule.py
+++ load/LoadEventSchedule.py
@@ -1,24 +1,54 @@
 import fastf1
 import boto3
 import datetime
-from F1DataIngestion import F1DataIngestion
+from load.data_loader import DataIngestion
+from logger import logger
 
 dynamodb = boto3.client('dynamodb')
 events_table = 'F1EventsSchedule'
 lambda_client = boto3.client('lambda')
 cloudwatch_events = boto3.client('events')
-bucket_name = 'your-s3-bucket'
+bucket_name = 'race-predictor-pro'
 prefix = 'f1_data'
+
+def create_dynamoDB_table():
+    try:
+        existing_table = dynamodb.list_tables()['TableNames']
+
+        if events_table not in existing_table:
+            dynamodb.create_table(
+                TableName=events_table,
+                KeySchema=[
+                    {'AttributeName': 'EventDate', 'KeyType': 'HASH'},
+                    {'AttributeName': 'EventName', 'KeyType': 'RANGE'}
+                ],
+                AttributeDefinitions=[
+                    {'AttributeName': 'EventDate', 'AttributeType': 'S'},
+                    {'AttributeName': 'EventName', 'AttributeType': 'S'}
+                ],
+                ProvisionedThroughput={
+                    'ReadCapacityUnits': 5,
+                    'WriteCapacityUnits': 5
+                }
+            )
+            logger.info('Created table'+events_table+'...')
+            boto3.get_waiter('table_exists').wait(TableName=events_table)
+            logger.info(f'Table {events_table} created successfully')
+
+    except Exception as e:
+        logger.error(f"Error creating table {e}")
 
 
 def load_event_schedule_to_dynamodb(start_year, end_year):
-    f1_ingestion = F1DataIngestion(bucket_name, prefix)
+    f1_ingestion = DataIngestion(bucket_name, prefix)
 
     for year in range(start_year, end_year + 1):
         schedule = fastf1.get_event_schedule(year)
         for _, row in schedule.iterrows():
             event_date = row['Session5DateUtc']
             event_name = row['EventName']
+            if row['EventFormat'] == 'testing':
+                continue
             event_processed = event_date < datetime.datetime.utcnow()
 
             dynamodb.put_item(
