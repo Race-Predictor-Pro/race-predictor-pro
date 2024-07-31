e4487656cd0efe1f5a40344cb52f21578b574099
parameshwarbhat411 <parameshwar.bhat@gwmail.gwu.edu>
2024-07-31 12:07:05 -0400

    logging setup updated to handle dynamic creation of files with runtime timestamp, file handler added to temp.conf

--- logger.py
+++ logger.py
@@ -1,6 +1,15 @@
-from logging_setup import setup_logging
+from logging_setup import LoggingSetup
 import logging
 
-setup_logging()
 
-logger = logging.getLogger('simpleLogger')
+class Logger:
+    @staticmethod
+    def get_logger(name='simpleLogger'):
+        # Ensure the logging setup is initialized
+        logging_setup = LoggingSetup()
+        return logging.getLogger(name)
+
+    @staticmethod
+    def get_log_file():
+        return LoggingSetup.get_log_file()
+
